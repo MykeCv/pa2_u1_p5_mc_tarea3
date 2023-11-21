cc58e67ca9b551cb7197dd3c296fc0f6b8f01288
MykeCv <mykecevallos@gmail.com>
2023-11-20 21:43:41 -0500

    Tarea 3

---
+++ src/main/java/com/uce/edu/Pa2U1P5McTarea3Application.java
@@ -0,0 +1,22 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5McTarea3Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5McTarea3Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("TAREA 3");
+		System.out.println("Soy Michael Cevallos");
+		System.out.println("Este es el cambio en la misma rama");
+	}
+
+}
